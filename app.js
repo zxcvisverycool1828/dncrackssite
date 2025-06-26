31604ec74aef0e8e7b616b2d994993c90f80997a
zxcvisverycool1828 <heightclient0@gmail.com>
2025-06-26 01:56:20 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 50,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.2,
+      "random": true,
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#333333",
+      "opacity": 0.2,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    },
+    "modes": {
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      }
+    }
+  },
+  "retina_detect": true
+});
